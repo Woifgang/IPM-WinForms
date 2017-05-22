6cce3ca19078238971d6612920062fd048d9c4f5
Woifgang Aschenbrenner <wolfgang.aschenbrenner@gmx.net>
2017-05-22 09:57:09 +0200

    Exceltabelle wird ausgelesen, Prüfung der Tabelle auf leere Zeilen

--- IPM_WinForms/Form1.Designer.cs
+++ IPM_WinForms/Form1.Designer.cs
@@ -28,12 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonOeffnen = new System.Windows.Forms.Button();
+            this.buttonAWLQuellcodeErzeugen = new System.Windows.Forms.Button();
+            this.exelListView = new System.Windows.Forms.ListView();
+            this.columnHeaderDatensatz = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderArbeitsfolge = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderBeschreibung = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderAufloesung = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderMerkmalKennung = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderMerkmalTyp = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderMerkmalEinheit = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderLfdNummer = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // buttonOeffnen
+            // 
+            this.buttonOeffnen.Location = new System.Drawing.Point(12, 12);
+            this.buttonOeffnen.Name = "buttonOeffnen";
+            this.buttonOeffnen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOeffnen.TabIndex = 0;
+            this.buttonOeffnen.Text = "Datei öffnen";
+            this.buttonOeffnen.UseVisualStyleBackColor = true;
+            this.buttonOeffnen.Click += new System.EventHandler(this.buttonOeffnen_Click);
+            // 
+            // buttonAWLQuellcodeErzeugen
+            // 
+            this.buttonAWLQuellcodeErzeugen.Location = new System.Drawing.Point(12, 402);
+            this.buttonAWLQuellcodeErzeugen.Name = "buttonAWLQuellcodeErzeugen";
+            this.buttonAWLQuellcodeErzeugen.Size = new System.Drawing.Size(148, 23);
+            this.buttonAWLQuellcodeErzeugen.TabIndex = 2;
+            this.buttonAWLQuellcodeErzeugen.Text = "AWL Quellcode erzeugen";
+            this.buttonAWLQuellcodeErzeugen.UseVisualStyleBackColor = true;
+            // 
+            // exelListView
+            // 
+            this.exelListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderLfdNummer,
+            this.columnHeaderDatensatz,
+            this.columnHeaderArbeitsfolge,
+            this.columnHeaderBeschreibung,
+            this.columnHeaderAufloesung,
+            this.columnHeaderMerkmalKennung,
+            this.columnHeaderMerkmalTyp,
+            this.columnHeaderMerkmalEinheit});
+            this.exelListView.Location = new System.Drawing.Point(12, 41);
+            this.exelListView.Name = "exelListView";
+            this.exelListView.Size = new System.Drawing.Size(760, 355);
+            this.exelListView.TabIndex = 3;
+            this.exelListView.UseCompatibleStateImageBehavior = false;
+            this.exelListView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeaderDatensatz
+            // 
+            this.columnHeaderDatensatz.DisplayIndex = 0;
+            this.columnHeaderDatensatz.Text = "Datensatz";
+            // 
+            // columnHeaderArbeitsfolge
+            // 
+            this.columnHeaderArbeitsfolge.DisplayIndex = 1;
+            this.columnHeaderArbeitsfolge.Text = "Arbeitsfolge";
+            this.columnHeaderArbeitsfolge.Width = 130;
+            // 
+            // columnHeaderBeschreibung
+            // 
+            this.columnHeaderBeschreibung.DisplayIndex = 2;
+            this.columnHeaderBeschreibung.Text = "Beschreibung";
+            this.columnHeaderBeschreibung.Width = 200;
+            // 
+            // columnHeaderAufloesung
+            // 
+            this.columnHeaderAufloesung.DisplayIndex = 3;
+            this.columnHeaderAufloesung.Text = "Auflösung (Float)";
+            this.columnHeaderAufloesung.Width = 100;
+            // 
+            // columnHeaderMerkmalKennung
+            // 
+            this.columnHeaderMerkmalKennung.DisplayIndex = 4;
+            this.columnHeaderMerkmalKennung.Text = "Merkmalkennung";
+            this.columnHeaderMerkmalKennung.Width = 100;
+            // 
+            // columnHeaderMerkmalTyp
+            // 
+            this.columnHeaderMerkmalTyp.DisplayIndex = 5;
+            this.columnHeaderMerkmalTyp.Text = "Typ";
+            // 
+            // columnHeaderMerkmalEinheit
+            // 
+            this.columnHeaderMerkmalEinheit.DisplayIndex = 6;
+            this.columnHeaderMerkmalEinheit.Text = "Einheit";
+            // 
+            // columnHeaderLfdNummer
+            // 
+            this.columnHeaderLfdNummer.Text = "Lfd Nr";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.exelListView);
+            this.Controls.Add(this.buttonAWLQuellcodeErzeugen);
+            this.Controls.Add(this.buttonOeffnen);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonOeffnen;
+        private System.Windows.Forms.Button buttonAWLQuellcodeErzeugen;
+        private System.Windows.Forms.ListView exelListView;
+        private System.Windows.Forms.ColumnHeader columnHeaderDatensatz;
+        private System.Windows.Forms.ColumnHeader columnHeaderArbeitsfolge;
+        private System.Windows.Forms.ColumnHeader columnHeaderBeschreibung;
+        private System.Windows.Forms.ColumnHeader columnHeaderAufloesung;
+        private System.Windows.Forms.ColumnHeader columnHeaderMerkmalKennung;
+        private System.Windows.Forms.ColumnHeader columnHeaderMerkmalTyp;
+        private System.Windows.Forms.ColumnHeader columnHeaderMerkmalEinheit;
+        private System.Windows.Forms.ColumnHeader columnHeaderLfdNummer;
     }
 }
 
--- IPM_WinForms/Form1.cs
+++ IPM_WinForms/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace IPM_WinForms
 {
@@ -16,5 +17,150 @@ namespace IPM_WinForms
         {
             InitializeComponent();
         }
+
+        private void buttonOeffnen_Click(object sender, EventArgs e)
+        {
+            string pfad = " ";
+            OpenFileDialog Import = new OpenFileDialog();
+            Import.Filter = "Excel-Arbeitsmappe (*.xlsx)|*.xlsx|All files (*.*)|*.*";
+
+            if (Import.ShowDialog() == DialogResult.OK )
+            {
+                pfad = Import.FileName;
+            }
+
+            leseExceltabelleEin(pfad);
+        }
+
+       private void leseExceltabelleEin(string pfad)
+        {
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+            excel.Visible = false;
+            Excel.Workbook wb = null;
+
+            
+            int i;
+            int zeileNrStart = 4;
+            int zeileNrEnde = 17;
+
+            string datensatz,                   // Spalte B
+                   arbeitsfolge,                // Spalte C
+                   beschreibung,                // Spalte D
+                   quelle,                      // Spalte E
+                   wertebereich,                // Spalte F
+                   aufloesung,                  // Spalte G
+                   merkmalKennung,              // Spalte H
+                   zusatzInfoMerkmal,           // Spalte I
+                   merkmalNummer,               // Spalte J
+                   merkmalTyp,                  // Spalte K
+                   merkmalEinheit,              // Spalte L
+                   statusKennung,               // Spalte M
+                   einheit,                     // Spalte N
+                   beispiel = "";               // Spalte O
+
+            wb = excel.Workbooks.Open(pfad);
+
+            Excel.Sheets sheets = wb.Worksheets;
+            Excel.Worksheet ws = (Excel.Worksheet)sheets.get_Item("IPM_ML12");
+
+           
+
+            try
+            {
+                //for (i = 6; i <= excel.get_Range("I" + excel.Rows.Count, "I" + excel.Rows.Count).get_End(Excel.XlDirection.xlUp).Row; i++)
+                for (i = zeileNrStart; i <= zeileNrEnde; i++)
+                
+                {
+                    Excel.Range datensatz_e = (Excel.Range)ws.get_Range("B" + i, "B" + i);
+                    Excel.Range arbeitsfolge_e = (Excel.Range)ws.get_Range("C" + i, "C" + i);
+                    Excel.Range beschreibung_e = (Excel.Range)ws.get_Range("D" + i, "D" + i);
+                    Excel.Range quelle_e = (Excel.Range)ws.get_Range("E" + i, "E" + i);
+                    Excel.Range wertebereich_e = (Excel.Range)ws.get_Range("F" + i, "F" + i);
+                    Excel.Range aufloesung_e = (Excel.Range)ws.get_Range("G" + i, "G" + i);
+                    Excel.Range merkmalKennung_e = (Excel.Range)ws.get_Range("H" + i, "H" + i);
+                    Excel.Range zusatzInfoMerkmal_e = (Excel.Range)ws.get_Range("I" + i, "I" + i);
+                    Excel.Range merkmalNummer_e = (Excel.Range)ws.get_Range("J" + i, "J" + i);
+                    Excel.Range merkmalTyp_e = (Excel.Range)ws.get_Range("K" + i, "K" + i);
+                    Excel.Range merkmalEinheit_e = (Excel.Range)ws.get_Range("L" + i, "L" + i);
+                    Excel.Range satusKennung_e = (Excel.Range)ws.get_Range("M" + i, "M" + i);
+                    Excel.Range einheit_e = (Excel.Range)ws.get_Range("N" + i, "N" + i);
+                    Excel.Range beispiel_e = (Excel.Range)ws.get_Range("O" + i, "O" + i);
+
+
+
+
+
+                    datensatz = PruefeExcelTabelle(datensatz_e);                 // Spalte B
+                    arbeitsfolge = PruefeExcelTabelle(arbeitsfolge_e);                // Spalte C
+                    beschreibung = PruefeExcelTabelle(beschreibung_e);                 // Spalte D
+                    quelle = PruefeExcelTabelle(quelle_e);                    // Spalte E
+                    wertebereich = PruefeExcelTabelle(wertebereich_e);                 // Spalte F
+                    aufloesung = PruefeExcelTabelle(aufloesung_e);                   // Spalte G
+                    merkmalKennung = PruefeExcelTabelle(merkmalKennung_e);               // Spalte H
+                    zusatzInfoMerkmal = PruefeExcelTabelle(zusatzInfoMerkmal_e);            // Spalte I
+                    merkmalNummer = PruefeExcelTabelle(merkmalNummer_e);                // Spalte J
+                    merkmalTyp = PruefeExcelTabelle(merkmalTyp_e);                   // Spalte K
+                    merkmalEinheit = PruefeExcelTabelle(merkmalEinheit_e);               // Spalte L
+                    statusKennung = PruefeExcelTabelle(satusKennung_e);                // Spalte M
+                    einheit = PruefeExcelTabelle(einheit_e);                      // Spalte N
+                    beispiel = PruefeExcelTabelle(beispiel_e);                // Spalte O
+
+                    
+                        ListViewItem item1 = new ListViewItem(); //(i.ToString(), 0);
+
+                        item1.SubItems.Add(datensatz);
+                        item1.SubItems.Add(arbeitsfolge);
+                        item1.SubItems.Add(beschreibung);
+                        item1.SubItems.Add(quelle);
+                        item1.SubItems.Add(wertebereich);
+                        item1.SubItems.Add(aufloesung);
+                        item1.SubItems.Add(merkmalKennung);
+                        item1.SubItems.Add(zusatzInfoMerkmal);
+                        item1.SubItems.Add(merkmalNummer);
+                        item1.SubItems.Add(merkmalTyp);
+                        item1.SubItems.Add(merkmalEinheit);
+                        item1.SubItems.Add(statusKennung);
+                        item1.SubItems.Add(einheit);
+                        item1.SubItems.Add(beispiel);
+
+                        exelListView.Items.AddRange(new ListViewItem[] { item1 });
+                    
+
+                    
+
+                    
+                }
+
+                // Add the ListView to the control collection.
+                this.Controls.Add(exelListView);
+                
+
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+            wb.Close();
+
+        }  
+        
+        private string PruefeExcelTabelle(Excel.Range tmp_e)
+        {
+            string tmp = "";
+
+            if (tmp_e.Value2 != null)
+            {
+
+                tmp = tmp_e.Value2.ToString();
+            }
+            else
+            {
+                tmp = "  ";
+            }
+            return tmp;
+        }    
+
     }
 }
